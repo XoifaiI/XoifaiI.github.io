a66ebc794fde0ee989c597f73f9541b6a62bc2ab
XNX <120916578+XoifaiI@users.noreply.github.com>
2025-06-15 02:00:19 +0100

    Create eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+      "react-hooks/exhaustive-deps": "warn",
+      "no-unused-vars": "warn",
+      "prefer-const": "warn"
+    }
+  }
+];
+
+export default eslintConfig;
